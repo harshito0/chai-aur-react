b6f1c8d94857df0e3dc1c89c86921897939449db
harshito0 <harshitsingh190622@gmail.com>
2026-03-25 00:02:33 +0530

    minicontext

--- ReactRouters/src/main.jsx
+++ ReactRouters/src/main.jsx
@@ -1,28 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import Layout from "./Layout.jsx";
 import Home from "../components/Home/Home.jsx";
 import About from "../components/About/About.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter(
+  createBrowserRouter(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
